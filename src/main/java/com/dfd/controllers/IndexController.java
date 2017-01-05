f114aca38c78f3359b338242a4fb4f25ace61e87
Edip YILDIZ <edip.yildiz@hotmail.com>
2017-01-05 08:36:05 +0200

    setting

---
+++ src/main/java/com/dfd/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.dfd.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	
+public IndexController() {
+}
+
+@RequestMapping("/index")
+private String index() {
+	
+	return "/jsp/index.jsp";
+
+}
+}
